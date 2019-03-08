976716ca54e0a157103ec81e2042deab36164512
Kostya Minkov <hallrizon@gmail.com>
2019-03-08 19:54:31 +0300

    Added header for home page, 2 modals auth and register, audit from front inputs in forms

---
+++ public/javascripts/home/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+    $('.tooltipped').tooltip();
+    $('.modal').modal();
+    $('.tabs').tabs();
+});
--- routes/home/index.js
+++ routes/home/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const Token = require('../../utils/Token');
 
-router.get('/', (req, res)=>{
-    return res.render('index.twig',{});
+router.get('/', (req, res) => {
+    let isAuth = false;
+    if (Token.getToken(req)) {
+        isAuth = true;
+    }
+
+    return res.render('index.twig', {
+        'isAuth': isAuth
+    });
 });
 
 module.exports = router;
